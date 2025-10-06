bf4b38b2fadd22c157f638b0c80b6f7e7e4d7251
Vraj-amipara <vrajamipara9@gmail.com>
2025-10-06 14:20:34 +0530

    first commit

--- java_lec_6/script.js
+++ java_lec_6/script.js
@@ -32,3 +32,43 @@ console.log(frEl);
 let allEl = document.querySelectorAll("p") // all element
 console.log(allEl);
 
+let headden_prag = document.querySelector(".hidden_p"); 
+// type = headden_prag
+// ans = <p class=​"hidden_p">​Lorem, ipsum dolor.one​</p>​
+// headden_prag.innerHTML
+// 'Lorem, ipsum dolor.one'
+// headden_prag.innerte
+// undefined
+
+
+// Qs. Create a H2 heading element with text - “Hello JavaScript”. Append “from Apna College students” to this text using JS.
+
+let h2 = document.querySelector("h2");
+// console.dir(h2.innerText); // hello JavaScript
+h2.innerText = h2.innerText + " from TC students";
+// h2.innerText
+// ' hello JavaScript from Apna College students'
+// h2.innerHTML
+// ' hello JavaScript from Apna College students'
+
+// Create 3 divs with common class name - “box”. Access them & add some unique text to each of them.
+
+let divs = document.getElementsByClassName("box");
+console.log(divs); // HTMLCollection(3) [div.box, div.box, div.box]
+
+let ind = 1
+for (div of divs) {
+    // console.log(div.innerText);
+    // console.log(div);
+
+    div.innerText = `new unique text ${ind}`;
+    ind ++;
+
+    // bigginar try  
+
+    divs[0] = "new unique text 1";//not used
+    divs[1] = "new unique text 2";//not used
+    divs[2] = "new unique text 3";//not used
+}
+
+// dom  part 2  -----------------------------------------------------------------------------
---
+++ java_lec_7/script.js
@@ -0,0 +1,11 @@
+let div = document.querySelector('div');
+console.log(div);
+
+let id = div.getAttribute('id');
+console.log(id);
+
+let para = document.querySelector(".para");
+console.log(para.getAttribute("class"));
+
+let p =document.querySelector("p");
+p.style.backgroundColor = "green";
